01375a755e9e5104e6c204f1276258db6c443f60
Jason <Jasonhan523@163.com>
2016-12-15 13:57:35 +0800

    111

--- app/src/main/java/com/myapp/MainActivity.java
+++ app/src/main/java/com/myapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView1 = (TextView) findViewById(R.id.textView1);
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
     }
 }
